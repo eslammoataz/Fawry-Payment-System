f6c201492784c5f7ef77cf3138f4fb807ff7dd45
Eslam ElMoataz <eslammezo2323@gmail.com>
2022-12-27 20:46:26 +0200

    added internet service etisalat

--- src/main/java/Demo/Payment/PaymentFactory.java
+++ src/main/java/Demo/Payment/PaymentFactory.java
@@ -1,7 +1,7 @@
 package Demo.Payment;
 
 public class PaymentFactory {
-    public Payment create(String choice){
+    public Payment create(String choice , MA){
         if(choice=="CreditCard")
             return new CreditCardPayment();
     }
