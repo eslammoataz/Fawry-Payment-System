b946bed6c97a421e6d0a4408b9ddf6dd3c086dc9
Eslam ElMoataz <eslammezo2323@gmail.com>
2022-12-29 14:17:28 +0200

    view refunds for admin

--- src/main/java/Demo/VeiwRefunds/Refund.java
+++ src/main/java/Demo/VeiwRefunds/Refund.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
-
+//making setter and geter so spring can access to log it on screen
 @Component
 @Getter
 @Setter
